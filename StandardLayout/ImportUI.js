cbe1eb3aa988dd97c08904526bc57dc568d4f530
kdinhN10 <kevindinh066@gmail.com>
2025-11-18 17:48:22 -0500

    Revert "Sync Sidebar to match"

--- StandardLayout/ImportUI.js
+++
@@ -1,6 +0,0 @@
-// Asigns the <aside> tag by the id and then copies the basic UI file.
-function loadUI(id, file) {
-    fetch(file)
-        .then(response => response.text())
-        .then(data => {document.getElementById(id).innerHTML = data;})
-}
